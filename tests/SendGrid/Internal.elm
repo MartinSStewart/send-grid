dd5b45297b9b466310622a6309047b0d3e56f06b
Martin Stewart <MartinSStewart@gmail.com>
2022-10-11 12:13:23 +0200

    Fix br tag

--- tests/SendGrid/Internal.elm
+++ tests/SendGrid/Internal.elm
@@ -1,6 +1,7 @@
 module SendGrid.Internal exposing (..)
 
 import Email.Html
+import Email.Html.Attributes
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Internal
@@ -9,16 +10,35 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    describe "The Email.HTML module" [
-        describe "Email.Html.toHtml"
+    describe "The Email.HTML module"
+        [ describe "Email.Html.toHtml"
             [ test "includes HTML after a <br>" <|
                 \_ ->
-                    Email.Html.div [] [Email.Html.text "line 1", Email.Html.br [] [], Email.Html.text "line 2"]
+                    Email.Html.div [] [ Email.Html.text "line 1", Email.Html.br [] [], Email.Html.text "line 2" ]
                         |> Internal.toString
                         |> Tuple.first
-                        |> String.contains "line 2"
-                        |> Expect.true "Expected the HTML to contain \"line 2\" "
-
-
+                        |> Expect.equal "<div>line 1<br>line 2</div>"
+            , test "Handle <br> with attributes" <|
+                \_ ->
+                    Email.Html.div
+                        []
+                        [ Email.Html.text "line 1"
+                        , Email.Html.br [ Email.Html.Attributes.color "red" ] []
+                        , Email.Html.text "line 2"
+                        ]
+                        |> Internal.toString
+                        |> Tuple.first
+                        |> Expect.equal "<div>line 1<br style=\"color: red\">line 2</div>"
+            , test "Handle <br> with children" <|
+                \_ ->
+                    Email.Html.div
+                        []
+                        [ Email.Html.text "line 1"
+                        , Email.Html.br [] [ Email.Html.text "inside br" ]
+                        , Email.Html.text "line 2"
+                        ]
+                        |> Internal.toString
+                        |> Tuple.first
+                        |> Expect.equal "<div>line 1<br>inside br</br>line 2</div>"
+            ]
         ]
-    ]
